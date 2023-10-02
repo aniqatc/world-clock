0e89d81023018f33cd860a5dbc5b07b22b13b533
aniqatc <aniqatc97@gmail.com>
2023-10-02 16:51:11 -0400

    Add functionality to analog clock with live time

--- script.js
+++ script.js
@@ -10,26 +10,34 @@ toggleTzButton.addEventListener('click', function () {
 });
 
 /* Format Timezone Name */
-function formatTzName(tz) {
-	return tz.split('/')[1].replace('_', ' ');
+function formatTzName(tz, i) {
+	return tz.split('/')[i].replace('_', ' ');
 }
 
 /* Time Card */
 const timeCardHeading = document.querySelector('.time-card--heading h1');
+
+/* Analog Clock */
 const clockDate = document.querySelector('.clock--date');
+const clockSecond = document.querySelector('.clock--hand-second');
+const clockMinute = document.querySelector('.clock--hand-minute');
+const clockHour = document.querySelector('.clock--hand-hour');
+
+/* Digital Clock */
 const digitalTime = document.querySelector('.clock--digital-time');
 const digitalSeconds = document.querySelector('.clock--digital-s');
 const digitalAM = document.querySelector('.clock--am');
 const digitalPM = document.querySelector('.clock--pm');
-const dateInfoHeading = document.querySelector('.tz-data-wrapper h3');
 
+/* Card Info Section */
+const dateInfoHeading = document.querySelector('.tz-data-wrapper h3');
 const tzTimeDifference = document.getElementById('tz-difference');
 const tzOfficialName = document.getElementById('tz-official-name');
 const tzDayYear = document.getElementById('tz-day-year');
 
 function updateTimeCard(tz) {
-	timeCardHeading.innerHTML = `${formatTzName(tz)}`;
-	clockDate.innerHTML = `${moment.tz(tz).format('MM[-]DD[-]YYYY')}`;
+	/* DIGITAL CLOCK */
+	timeCardHeading.innerHTML = `${formatTzName(tz, 1)}`;
 	digitalTime.innerHTML = `${moment.tz(tz).format('hh[:]mm')}`;
 	digitalSeconds.innerHTML = `${moment.tz(tz).format('[:]ss')}`;
 
@@ -42,6 +50,7 @@ function updateTimeCard(tz) {
 		digitalAM.classList.add('active');
 	} else digitalPM.classList.add('active');
 
+	/* ADDITIONAL DATA/INFO SECTION */
 	dateInfoHeading.innerHTML = `${moment
 		.tz(tz)
 		.format('dddd[,] MMMM Do[,] YYYY')}`;
@@ -67,4 +76,18 @@ function updateTimeCard(tz) {
 	tzTimeDifference.innerHTML = 'Not yet...';
 }
 
+function updateAnalogClock(tz) {
+	let seconds = (moment.tz(tz).format('s') / 60) * 360;
+	let minutes = (moment.tz(tz).format('m') / 60) * 360;
+	let hours = moment.tz(tz).format('h') * 30;
+
+	clockDate.innerHTML = `${moment.tz(tz).format('MM[-]DD[-]YYYY')}`;
+	clockSecond.style.transform = `rotate(${seconds}deg)`;
+	clockMinute.style.transform = `rotate(${minutes}deg)`;
+	clockHour.style.transform = `rotate(${hours}deg)`;
+}
+
 updateTimeCard(moment.tz.guess());
+updateAnalogClock(moment.tz.guess());
+
+setInterval(() => updateAnalogClock(moment.tz.guess()), 1000);
