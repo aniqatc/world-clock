b221961b650de58e3c3790762a007e2482b9165c
aniqatc <aniqatc97@gmail.com>
2023-11-04 00:44:19 -0400

    script.js: minor cleanup

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-/**************
-All Necessary Declarations 
-**************/
+// Button to Toggle Dropdown of All Timezones Options
+const tzDropdown = document.getElementById('tz-dropdown');
+const toggleTzButton = document.getElementById('list-tz-btn');
+const userTzButton = document.getElementById('user-tz-btn');
 
-/* Time Card General */
+// Button to Generate Random Timezones
+const shuffleButton = document.getElementById('shuffle-tz-btn');
+
+// Timezone Button Wrapper - For Dynamically Generated Buttons
+const timezoneButtonWrapper = document.querySelector('.timezone-button-group');
+
+// Time Card General
 const timeCardHeading = document.querySelector('.time-card--heading h1');
 
-/* Time Card: Analog Clock */
-const clockDate = document.querySelector('.clock--date');
-const clockSecond = document.querySelector('.clock--hand-second');
-const clockMinute = document.querySelector('.clock--hand-minute');
-const clockHour = document.querySelector('.clock--hand-hour');
-
-/* Time Card: Digital Clock */
-const digitalTime = document.querySelector('.clock--digital-time');
-const digitalSeconds = document.querySelector('.clock--digital-s');
-const digitalAM = document.querySelector('.clock--am');
-const digitalPM = document.querySelector('.clock--pm');
-
-/* Time Card Info Section */
+// Time Card Info Section
 const dateInfoHeading = document.querySelector('.tz-data-wrapper h3');
 const tzDaylightSavings = document.getElementById('tz-daylight');
 const tzOfficialName = document.getElementById('tz-official-name');
@@ -25,22 +20,23 @@ const tzDayYear = document.getElementById('tz-day-year');
 const tzRegion = document.getElementById('tz-region');
 const tzGoogle = document.getElementById('tz-google');
 
-/* Variables To Be Used As IDs To Clear Intervals */
+// Time Card: Digital Clock
+const digitalTime = document.querySelector('.clock--digital-time');
+const digitalSeconds = document.querySelector('.clock--digital-s');
+const digitalAM = document.querySelector('.clock--am');
+const digitalPM = document.querySelector('.clock--pm');
+
+// Time Card: Analog Clock
+const clockDate = document.querySelector('.clock--date');
+const clockSecond = document.querySelector('.clock--hand-second');
+const clockMinute = document.querySelector('.clock--hand-minute');
+const clockHour = document.querySelector('.clock--hand-hour');
+
+// Variables To Be Used As IDs To Clear Intervals
 let digitalClockInterval;
 let analogClockInterval;
 
-/* Button to Toggle Dropdown of All Timezones Options */
-const tzDropdown = document.getElementById('tz-dropdown');
-const toggleTzButton = document.getElementById('list-tz-btn');
-const userTzButton = document.getElementById('user-tz-btn');
-
-/* Button to Generate 30 New Timezones */
-const shuffleButton = document.getElementById('shuffle-tz-btn');
-
-/* Timezone Button Wrapper That Will Hold All 30 New Buttons */
-const timezoneButtonWrapper = document.querySelector('.timezone-button-group');
-
-/* Popular Timezone Names */
+// Popular Timezone Names
 const abbrs = {
 	ACDT: 'ACDT, Australian Central Daylight Time',
 	ACST: 'ACST, Australian Central Standard Time',
@@ -104,27 +100,24 @@ const abbrs = {
 };
 
 /*************************
- Main Functionality
+Functionality ************
 *************************/
 
-/* Generate content based on last selected timezone
-or generate content based on user's current timezone */
-const savedLocation = localStorage.getItem('location');
-if (savedLocation) {
-	updateTimeCard(savedLocation);
-	updateAnalogClock(savedLocation);
-} else {
-	updateTimeCard(moment.tz.guess());
-	updateAnalogClock(moment.tz.guess());
+// Initial timezone
+function renderUserLocation() {
+	const savedLocation = localStorage.getItem('location');
+	if (savedLocation) {
+		updateTimeCard(savedLocation);
+		updateAnalogClock(savedLocation);
+	} else {
+		updateTimeCard(moment.tz.guess());
+		updateAnalogClock(moment.tz.guess());
+	}
 }
 
-/* Generate content based on user's selected option on dropdown menu */
-tzDropdown.addEventListener('change', function (event) {
-	updateTimeCard(event.target.value);
-	updateAnalogClock(event.target.value);
-});
+renderUserLocation();
 
-/* Toggle dropdown menu of timezones */
+// Timezone Dropdown List
 toggleTzButton.addEventListener('click', function () {
 	if (tzDropdown.style.display === 'block') {
 		tzDropdown.style.display = 'none';
@@ -133,26 +126,37 @@ toggleTzButton.addEventListener('click', function () {
 	}
 });
 
-/* Shuffle button that generates 30 new timezones */
+tzDropdown.addEventListener('change', function (event) {
+	updateTimeCard(event.target.value);
+	updateAnalogClock(event.target.value);
+});
+
+// Shuffle Timezones Button
 shuffleButton.addEventListener('click', function () {
+	generateRandomTzButtons();
+
 	document
 		.querySelector('.timezone-wrapper')
 		.scrollIntoView({ behavior: 'smooth' });
-	generateRandomTzButtons();
 });
 
-/* Reset back to user's timezone */
+// Current Location Button
 userTzButton.addEventListener('click', function () {
 	updateTimeCard(moment.tz.guess());
 	updateAnalogClock(moment.tz.guess());
 
-	const newlyGeneratedBtns = timezoneButtonWrapper.querySelectorAll('button');
-	newlyGeneratedBtns.forEach(el => el.classList.remove('active'));
+	removeActiveTzButtons();
 });
 
-/* Get all timezones into an array */
+// Remove active from timezone buttons
+function removeActiveTzButtons() {
+	const newlyGeneratedBtns = timezoneButtonWrapper.querySelectorAll('button');
+	newlyGeneratedBtns.forEach(el => el.classList.remove('active'));
+}
+
+/* All timezones into an array */
 // Remove any timezones that do not have a region defined (e.g. GMT+100 should not be included)
-const filteredTz = moment.tz
+const allTimezones = moment.tz
 	.names()
 	.filter(
 		timezone =>
@@ -161,57 +165,51 @@ const filteredTz = moment.tz
 			!timezone.includes('-')
 	);
 
-/* Format timezone name */
+// Format timezone name
 function formatTzName(tz, i) {
 	return tz.split('/')[i].replace(/_/g, ' ');
 }
 
-/* Get random index to be used in timezone array */
+// Random index for timezone array
 function getRandomTz(array) {
 	const randomIndex = Math.floor(Math.random() * array.length);
 	return array[randomIndex];
 }
 
-/* Add timezones to dropdown selection (tzDropdown) */
-for (const timezone of filteredTz) {
+// Add timezones to dropdown
+for (const timezone of allTimezones) {
 	const option = document.createElement('option');
 	option.value = timezone;
 	option.textContent = timezone;
 	tzDropdown.appendChild(option);
 }
 
-/* Generate 30 random buttons that represent 30 different timezones */
+// Generate 30 unique timezone buttons
 function generateRandomTzButtons() {
-	/* Clear it everytime new buttons need to be generated */
 	timezoneButtonWrapper.innerHTML = '';
 
-	/* Get 30 unique timezones into a single array */
 	const uniqueTimezones = [];
 	while (uniqueTimezones.length < 30) {
-		const randomTz = getRandomTz(filteredTz);
+		const randomTz = getRandomTz(allTimezones);
 		if (randomTz !== 'undefined') {
 			uniqueTimezones.push(randomTz);
 		}
 	}
 
-	/* For each unique timezone, a button is generated */
 	for (const timezone of uniqueTimezones) {
 		const button = document.createElement('button');
 		function addButtonContent() {
 			button.innerHTML = `
-		<p>${formatTzName(timezone, 1)}</p><p>${formatTzName(timezone, 0)}</p>
-		<span>${moment.tz(timezone).format('hh[:]mm[:]ss A')}</span>
-		`;
+			<p>${formatTzName(timezone, 1)}</p><p>${formatTzName(timezone, 0)}</p>
+			<span>${moment.tz(timezone).format('hh[:]mm[:]ss A')}</span>
+			`;
 		}
 		addButtonContent();
 		setInterval(addButtonContent, 1000);
 		timezoneButtonWrapper.appendChild(button);
 
-		/* Button functionality to update Time Card content + add/remove active class */
 		button.addEventListener('click', () => {
-			const newlyGeneratedBtns =
-				timezoneButtonWrapper.querySelectorAll('button');
-			newlyGeneratedBtns.forEach(el => el.classList.remove('active'));
+			removeActiveTzButtons();
 
 			button.classList.add('active');
 			updateTimeCard(timezone);
@@ -222,18 +220,51 @@ function generateRandomTzButtons() {
 	}
 }
 
-/* Generate 30 random timezones by default */
 generateRandomTzButtons();
 
-/* Update all content in Time Card except analog clock */
+// Update Time Card content
 function updateTimeCard(tz) {
-	/* Save location to retrieve on next visit */
 	localStorage.setItem('location', tz);
 
-	/* Clear previous location's interval to prevent conflicts with new intervals */
 	clearInterval(digitalClockInterval);
 
-	/* DIGITAL CLOCK */
+	updateDigitalClock(tz);
+
+	// Override Moment's default abbreviations
+	moment.fn.zoneName = function () {
+		const abbr = this.zoneAbbr();
+		return abbrs[abbr] || abbr;
+	};
+
+	if (
+		moment.tz(tz).format('zz').includes('+') ||
+		moment.tz(tz).format('zz').includes('-')
+	) {
+		tzOfficialName.innerHTML = `
+        Time Offset: <span> ${moment.tz(tz).format('zz')}</span>`;
+	} else {
+		tzOfficialName.innerHTML = `
+        Timezone: <span> ${moment.tz(tz).format('zz')}</span>`;
+	}
+
+	dateInfoHeading.innerHTML = `${moment
+		.tz(tz)
+		.format('dddd[,] MMMM Do[,] YYYY')}`;
+	tzDayYear.innerHTML = `${moment.tz(tz).format('DDDo')}`;
+	tzRegion.innerHTML = `${formatTzName(tz, 0)}`;
+	tzGoogle.innerHTML = `<a href="https://www.google.com/search?q=
+		${formatTzName(tz, 1)}, ${formatTzName(tz, 0)}"
+		target="_blank"
+		rel="noopener noreferrer"
+		>Search on <i class="fa-brands fa-google"></i> 
+		about ${formatTzName(tz, 1)}</a>`;
+
+	moment.tz(tz).isDST() === true
+		? (tzDaylightSavings.innerHTML = `Active`)
+		: (tzDaylightSavings.innerHTML = 'Inactive');
+}
+
+function updateDigitalClock(tz) {
 	timeCardHeading.innerHTML = `${formatTzName(tz, 1)}`;
 	digitalTime.innerHTML = `${moment.tz(tz).format('hh[:]mm')}`;
 	digitalSeconds.innerHTML = `${moment.tz(tz).format('[:]ss')}`;
@@ -250,59 +281,11 @@ function updateTimeCard(tz) {
 		digitalAM.classList.remove('active');
 		digitalPM.classList.add('active');
 	}
-
-	/* ADDITIONAL DATA/INFO SECTION */
-	/* Override Moment's default abbreviations */
-	moment.fn.zoneName = function () {
-		const abbr = this.zoneAbbr();
-		return abbrs[abbr] || abbr;
-	};
-
-	/* Show Time Offset OR Timezone depending on the information available */
-	if (
-		moment.tz(tz).format('zz').includes('+') ||
-		moment.tz(tz).format('zz').includes('-')
-	) {
-		tzOfficialName.innerHTML = `
-        Time Offset: <span> ${moment.tz(tz).format('zz')}</span>`;
-	} else {
-		tzOfficialName.innerHTML = `
-        Timezone: <span> ${moment.tz(tz).format('zz')}</span>`;
-	}
-
-	/* Readable date */
-	dateInfoHeading.innerHTML = `${moment
-		.tz(tz)
-		.format('dddd[,] MMMM Do[,] YYYY')}`;
-
-	/* Current # of day in the year (out of 365) */
-	tzDayYear.innerHTML = `${moment.tz(tz).format('DDDo')}`;
-
-	/* Timezone Region */
-	tzRegion.innerHTML = `${formatTzName(tz, 0)}`;
-
-	/* Link to search more about the selected location on Google */
-	tzGoogle.innerHTML = `<a href="https://www.google.com/search?q=
-		${formatTzName(tz, 1)}, ${formatTzName(tz, 0)}"
-		target="_blank"
-		rel="noopener noreferrer"
-		>Search on <i class="fa-brands fa-google"></i> 
-		about ${formatTzName(tz, 1)}</a>`;
-
-	/* Prints whether or not that location is currently in daylight savings */
-	if (moment.tz(tz).isDST() === true) {
-		tzDaylightSavings.innerHTML = `Active`;
-	} else {
-		tzDaylightSavings.innerHTML = 'Inactive';
-	}
 }
 
-/* Animate analog clock with current timezone's time */
 function updateAnalogClock(tz) {
-	/* Clear previous location's interval to prevent conflicts with new intervals */
 	clearInterval(analogClockInterval);
 
-	/* Time converted to match movement around a circle */
 	let seconds = (moment.tz(tz).format('s') / 60) * 360;
 	let minutes = (moment.tz(tz).format('m') / 60) * 360;
 	let hours = moment.tz(tz).format('h') * 30;
